15b2f45c003b2dd4d866e842c1812eb2b8582297
cstadler <cstadler@informatik.uni-leipzig.de>
2013-01-30 21:54:14 +0100

    Removed js files

--- src/main/ts/org/aksw/semmap/app/main.js
+++
@@ -1,6 +0,0 @@
-function greeter(person) {
-    return "Hello, " + person;
-}
-var qe = new backend.QueryExecutionFactoryHttp("http://localhost", []);
-var user = "Jane User";
-document.body.innerHTML = greeter(user);
--- src/main/ts/org/aksw/sparql/backend/all.ts
+++ src/main/ts/org/aksw/sparql/backend/all.ts
@@ -1,3 +1,13 @@
 ///<reference path='./QueryExecution.ts'/>
 ///<reference path='./QueryExecutionFactory.ts'/>
 ///<reference path='./QueryExecutionFactoryHttp.ts'/>
+
+export module org {
+	export module aksw {
+		export module sparql {
+			export module backend {
+				
+			}
+		}
+	}
+}
